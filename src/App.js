b6c7332b038f80367b241a60b16a167b07a87c47
Bitnik212 <Bit@DESKTOP-L8PS63V>
2021-02-13 02:16:27 +0300

    почистил мусор и накинул немного стилей

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './views/pages/Home'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.main}>
+      <Home />
     </div>
   );
 }
 
-export default App;
+const styles = {
+  main: {
+    background: "black",
+    color: "white",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: 'center',
+  }
+}
